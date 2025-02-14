bae3bc8542859542a84fd0dbcdb7fcd089468835
maimohammed928 <maimohammed928@gmail.com>
2025-02-15 01:05:33 +0200

    contact page

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Form = () => {
     return (
-        <form className='px-10 py-10'>
+        <form className='px-5 lg:px-10 py-10'>
             <div className='lg:flex w-full gap-10 my-4'>
-                <div className='w-1/2'>
+                <div className='lg:w-1/2'>
                     <label className='font-semibold text-black-medium  py-2 block'>First Name</label>
                     <input className='border-[1px] rounded-[4px] shadow-sm w-full py-1 px-2' placeholder='Ahmed' />
                 </div>
 
-                <div className='w-1/2'>
+                <div className='lg:w-1/2'>
                     <label className='font-semibold text-black-medium py-2 block'>Last Name</label>
                     <input className='border-[1px] rounded-[4px] shadow-sm w-full py-1 px-2' placeholder='Mohammed' />
                 </div>
@@ -17,12 +17,12 @@ const Form = () => {
             </div>
 
             <div className='lg:flex w-full gap-10 my-4'>
-                <div className='w-1/2'>
-                    <label className=' text-black-medium font-semibold text-black-medium  py-2 block'>Email</label>
+                <div className='lg:lg:w-1/2'>
+                    <label className='  font-semibold text-black-medium  py-2 block'>Email</label>
                     <input className='border-[1px] rounded-[4px] shadow-sm w-full py-1 px-2' placeholder='Ahmed@gmail.com' />
                 </div>
 
-                <div className='w-1/2'>
+                <div className='lg:w-1/2'>
                     <label className='font-semibold text-black-medium py-2 block'>Phone Number</label>
                     <input className='border-[1px] rounded-[4px] shadow-sm w-full py-1 px-2' placeholder='+20 1999 99 9999' />
                 </div>
@@ -30,11 +30,11 @@ const Form = () => {
             </div>
             <div className='py-3'>
                 <label className='text-black-medium block font-semibold'>Select Subject ?</label>
-                <div className='flex  py-3'>
-                    <input className='me-2' type='checkbox' /> <span className='me-12  text-gray-800'>General Inquiry</span>
-                    <input className='me-2' type='checkbox' /> <span className='me-12  text-gray-800'>Test Packages</span>
-                    <input className='me-2' type='checkbox' />  <span className='me-12  text-gray-800'>Help & Support</span>
-                    <input className='me-2' type='checkbox' /> <span className='me-12  text-gray-800'>Report </span>
+                <div className='lg:flex  lg:flex-row flex flex-col  items-start  py-3'>
+                   <div> <input className='lg:me-2 ' type='checkbox' /> <span className='lg:me-12  text-gray-800'>General Inquiry</span> </div> 
+                  <div> <input className='lg:me-2' type='checkbox' /> <span className='lg:me-12  text-gray-800'>Test Packages</span> </div> 
+                  <div>   <input className='lg:me-2' type='checkbox' />  <span className='lg:me-12  text-gray-800'>Help & Support</span></div> 
+                  <div>   <input className='lg:me-2' type='checkbox' /> <span className='lg:me-12  text-gray-800'>Report </span></div> 
 
                 </div>
 
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -9,27 +9,27 @@ import Form from '../Components/Form'
 
 const ContactUs = () => {
   return (
-    <section className='py-20 lg:py-28  px-3 lg:px-32'>
-      <div className='relative z-10 ' >
-        <img className='absolute w-[170px] h-[170px] -start-20  -top-20 z-0' src={circle1} />
-        <img className='absolute w-[170px] h-[170px] -bottom-16 -end-20 z-0' src={circle2} />
+    <section className='py-20 lg:py-28  px-5 lg:px-32'>
+      <div className='relative z-5 ' >
+        <img className='absolute  w-0 lg:w-[170px] lg:h-[170px]  lg:-start-20  lg:-top-20 z-0' src={circle1} />
+        <img className='absolute w-0 lg:w-[170px] lg:h-[170px]   lg:-bottom-16 lg:-end-20 z-0' src={circle2} />
 
 
-        <div className=' relative  flex w-full border-[1px] p-2 bg-white border-gray-300 rounded-lg '>
+        <div className=' relative  lg:flex w-full border-[1px] p-2 bg-white border-gray-300 rounded-lg '>
 
           {/* info */}
-          <div className=' w-[35%] bg-Primary  text-white rounded-lg '>
+          <div className=' w-full lg:w-[35%] bg-Primary  text-white rounded-lg '>
             <div className=' pt-10 ps-10'>
-              <h3 className='pb-10 text-3xl font-[500] '>Contact Information</h3>
+              <h3 className='pb-10 text-2xl lg:text-3xl font-[500] '>Contact Information</h3>
               <div className='flex  gap-5'><img src={call} /> <p className='text-[16px] '>+20 1204944172 </p> </div>
               <div className='flex my-5 gap-5'><img src={mail} /> <p className='text-[16px] '>mohammed@gmail.com </p> </div>
               <div className='flex gap-5'><img src={address} /> <p className='text-[16px] '>Benha ,Al-Qalyubia </p> </div>
             </div>
 
-            <img className='w-[800px] h-auto' src={wave} />
+            <img className=' w-[300px] h-[300px] lg:w-[800px] lg:h-auto' src={wave} />
           </div>
           {/* form */}
-          <div className='w-[65%] '>
+          <div className='w-full lg:w-[65%] '>
             <Form />
           </div>
 

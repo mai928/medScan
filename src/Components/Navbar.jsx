061660f0d8629da4bd6c689af9216d0b6d4e70f6
maimohammed928 <maimohammed928@gmail.com>
2025-02-21 00:30:58 +0200

    navbar username

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -69,7 +69,38 @@ const Navbar = () => {
               <img className='cursor-pointer' onClick={() => setToggle(!toggle)} width={24} height={24} src={menu} />
               <img width={71} height={36} src={logo} />
             </div>
-            <RegistrationBtns gap='gap-1' className='px-3 py-1 text-[12px] font-[500]' />
+
+
+            <div className='relative'>
+              {
+                user ? (
+
+                  <button onClick={toggleDropdown} className=' cursor-pointer flex items-center gap-2    px-4 py-2'>
+                    <svg className='fill-Primary mb-2' width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8l256 0c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z" /></svg>
+                    <p className='text-[14px] font-semibold capitalize'>{username?.slice(1,-1) || null}</p>
+                    <p className='font-bold bg-Primary px-[10px] py-1 text-white rounded-full shadow-xl'>{username?.charAt(1).toUpperCase()}</p>
+                  </button>
+
+
+
+                ) : (<RegistrationBtns gap='gap-1' className='px-3 py-1 text-[12px] font-[500]' />
+                )
+              }
+
+              <div>
+                {
+                  isDropdownOpen &&
+                  <div className='z-30 absolute top-[66px] bg-white w-full cursor-pointer  border-2  text-center'>
+
+                    <button onClick={handlelogout} className='   text-black-medium font-semibold py-2  text-[15px] '>Logout</button>
+
+
+                  </div>
+                }
+              </div>
+
+            </div>
+
           </div>
 
 
@@ -96,7 +127,7 @@ const Navbar = () => {
 
                   <button onClick={toggleDropdown} className=' cursor-pointer flex items-center gap-3    px-4 py-2'>
                     <svg className='fill-Primary mb-2' width={25} height={25} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8l256 0c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z" /></svg>
-                    <p className='text-[14px] font-semibold'>{username || null}</p>
+                    <p className='text-[14px] font-semibold capitalize'>{username?.slice(1,-1) || null}</p>
                     <p className='font-bold bg-Primary px-3 py-2 text-white rounded-full shadow-xl'>{username?.charAt(1).toUpperCase()}</p>
                   </button>
 
